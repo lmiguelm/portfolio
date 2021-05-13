d572a39c109170444222f134f7822c6af10d229e
lmiguelm <luismiguelfernandes.marcelo@gmail.com>
2021-05-13 11:33:04 -0300

    Links

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -60,59 +60,25 @@ export default class MyDocument extends Document {
           />
 
           <meta name="twitter:card" content="summary" />
-          <meta name="twitter:url" content="https://yourdomain.com" />
-          <meta name="twitter:title" content="PWA App" />
-          <meta name="twitter:description" content="Best PWA App in the world" />
-          <meta
-            name="twitter:image"
-            content="https://yourdomain.com/static/icons/android-chrome-192x192.png"
-          />
-          <meta name="twitter:creator" content="@DavidWShadow" />
+          <meta name="twitter:url" content="https://lmiguelm.vercel.app" />
+          <meta name="twitter:title" content="Luis Miguel" />
+          <meta name="twitter:description" content="Portfólio de Luis Miguel" />
+          <meta name="twitter:image" content="/static/favicon.png" />
+          <meta name="twitter:creator" content="@lmiguelm" />
           <meta property="og:type" content="website" />
-          <meta property="og:title" content="PWA App" />
-          <meta property="og:description" content="Best PWA App in the world" />
-          <meta property="og:site_name" content="PWA App" />
-          <meta property="og:url" content="https://yourdomain.com" />
-          <meta
-            property="og:image"
-            content="https://yourdomain.com/static/icons/apple-touch-icon.png"
-          />
+          <meta property="og:title" content="Luis Miguel" />
+          <meta property="og:description" content="Portfólio de Luis Miguel" />
+          <meta property="og:site_name" content="Luis Miguel" />
+          <meta property="og:url" content="https://lmiguelm.vercel.app" />
+          <meta property="og:image" content="/static/favicon.png" />
 
-          <link
-            rel="apple-touch-startup-image"
-            href="/static/images/apple_splash_2048.png"
-            sizes="2048x2732"
-          />
-          <link
-            rel="apple-touch-startup-image"
-            href="/static/images/apple_splash_1668.png"
-            sizes="1668x2224"
-          />
-          <link
-            rel="apple-touch-startup-image"
-            href="/static/images/apple_splash_1536.png"
-            sizes="1536x2048"
-          />
-          <link
-            rel="apple-touch-startup-image"
-            href="/static/images/apple_splash_1125.png"
-            sizes="1125x2436"
-          />
-          <link
-            rel="apple-touch-startup-image"
-            href="/static/images/apple_splash_1242.png"
-            sizes="1242x2208"
-          />
-          <link
-            rel="apple-touch-startup-image"
-            href="/static/images/apple_splash_750.png"
-            sizes="750x1334"
-          />
-          <link
-            rel="apple-touch-startup-image"
-            href="/static/images/apple_splash_640.png"
-            sizes="640x1136"
-          />
+          <link rel="apple-touch-startup-image" href="/static/favicon.png" sizes="2048x2732" />
+          <link rel="apple-touch-startup-image" href="/static/favicon.png" sizes="1668x2224" />
+          <link rel="apple-touch-startup-image" href="/static/favicon.png" sizes="1536x2048" />
+          <link rel="apple-touch-startup-image" href="/static/favicon.png" sizes="1125x2436" />
+          <link rel="apple-touch-startup-image" href="/static/favicon.png" sizes="1242x2208" />
+          <link rel="apple-touch-startup-image" href="/static/favicon.png" sizes="750x1334" />
+          <link rel="apple-touch-startup-image" href="/static/favicon.png" sizes="640x1136" />
 
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link

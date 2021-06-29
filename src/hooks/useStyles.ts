2df61b19e2ea6115930ff67bb125ff6081b10fbe
lmiguelm <luismiguelfernandes.marcelo@gmail.com>
2021-06-29 14:49:19 -0300

    Remove

--- src/hooks/useStyles.ts
+++
@@ -1,7 +0,0 @@
-import { useContext } from 'react';
-import { StylesContext } from '../contexts/StylesContext';
-
-export function useStylesContext() {
-  const useStylesContext = useContext(StylesContext);
-  return useStylesContext;
-}
